1272a5605d11e43b31cd68f0e3bbe92edd25e075
Josef Dohnal <josef.dohnal@mepatek.cz>
2017-08-05 17:57:59 +0200

    Add empty role controls

---
+++ src/UserManager/UI/RoleFormFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pepa
+ * Date: 05.08.2017
+ * Time: 17:55
+ */
+
+namespace Mepatek\UserManager\UI;
+
+
+class RoleFormFactory
+{
+
+}
---
+++ src/UserManager/UI/Roles/RolesListControl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pepa
+ * Date: 05.08.2017
+ * Time: 17:56
+ */
+
+namespace Mepatek\UserManager\UI\Roles;
+
+
+class RolesListControl
+{
+
+}
